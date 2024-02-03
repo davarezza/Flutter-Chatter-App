c802c30dcd2d81be5d7b198cd7d3050d6fdfc610
davarezza <dapva2728@gmail.com>
2024-02-04 06:10:32 +0700

    Feat: Create appbar with avatar and icon button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,19 @@
+import 'package:chatter/helpers.dart';
 import 'package:chatter/pages/calls_page.dart';
 import 'package:chatter/pages/contacts_page.dart';
 import 'package:chatter/pages/messages_page.dart';
 import 'package:chatter/pages/notifications_page.dart';
 import 'package:chatter/theme.dart';
+import 'package:chatter/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/avatar.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
 
   final ValueNotifier<int> pageIndex = ValueNotifier(0);
+  final ValueNotifier<String> title = ValueNotifier('MessagesPage');
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -22,15 +27,56 @@ class _HomeScreenState extends State<HomeScreen> {
     ContactsPage()
   ];
 
+  final titlePages = const [
+    'Messages',
+    'Notification',
+    'Calls',
+    'Contacts'
+  ];
+
   var index = 0;
+  final ValueNotifier<String> title = ValueNotifier('Messages');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ValueListenableBuilder(
+          valueListenable: title,
+          builder: (BuildContext context, String value, _) {
+            return Text(
+              value,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            );
+          },
+        ),
+        leadingWidth: 54,
+        leading: Align(
+          alignment: Alignment.centerRight,
+          child: IconBackground(
+            icon: Icons.search,
+            onTap: () {
+              print('ToDO search');
+            },
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Avatar.small(url: Helpers.randomPictureUrl()),
+          )
+          ],
+      ),
       body: pages[index],
       bottomNavigationBar: _BottomNavigationBar(
         onItemSelected: (i) {
           setState(() {
             index = i;
+            title.value = titlePages[i];
           });
         },
       ),
