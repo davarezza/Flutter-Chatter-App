8a59752450c29c7cf83174e39c340508c83015eb
davarezza <dapva2728@gmail.com>
2024-02-04 20:55:59 +0700

    Feat: Add UI for message page

--- lib/pages/messages_page.dart
+++ lib/pages/messages_page.dart
@@ -1,9 +1,11 @@
 import 'package:chatter/helpers.dart';
+import 'package:chatter/models/message_data.dart';
 import 'package:chatter/models/story_data.dart';
 import 'package:chatter/theme.dart';
 import 'package:chatter/widgets/widgets.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({super.key});
@@ -18,17 +20,69 @@ class MessagesPage extends StatelessWidget {
         SliverList(
           delegate:
            SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Text('test');
-          },
+            _delegate
          ),
         )
       ],
     );
   }
+
+  Widget _delegate(BuildContext context, int index) {
+    final Faker faker = Faker();
+    final date = Helpers.randomDate();
+    return _MessageTitle(
+      messageData: MessageData(
+        senderName: faker.person.name(),
+        message: faker.lorem.sentence(),
+        messageDate: date,
+        dateMessage: Jiffy.parseFromDateTime(date).fromNow(),
+        profilePicture: Helpers.randomPictureUrl(),
+      ),
+    );
+  }
 }
 
+class _MessageTitle extends StatelessWidget {
+  const _MessageTitle({
+    Key? key, 
+    required this.messageData}) : super(key: key);
 
+  final MessageData messageData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Avatar.medium(url: messageData.profilePicture),
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              messageData.senderName,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                letterSpacing: 0.2,
+                wordSpacing: 1.5,
+                fontWeight: FontWeight.w900,
+              ), 
+             ),
+            SizedBox(
+              height: 20,
+              child: Text(
+                messageData.message,
+                overflow: TextOverflow.ellipsis,
+              )
+            ),
+          ],
+        ),
+      )
+    ],
+   );
+  }
+}
 
 class _Stories extends StatelessWidget {
   const _Stories({Key? key}) : super(key: key);
@@ -38,12 +92,12 @@ class _Stories extends StatelessWidget {
     return Card(
       elevation: 0,
       child: SizedBox(
-        height: 134,
+        height: 140,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Padding(
-              padding: EdgeInsets.only(left: 16.0, top: 8, bottom: 16),
+              padding: EdgeInsets.only(left: 16.0, top: 4, bottom: 16.0),
               child: const Text(
                 'Stories',
                 style: TextStyle(
